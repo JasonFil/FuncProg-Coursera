bbf750c79895b2951931ebad60b888943546c80e
Jason Filippou <jason.filippou@gmail.com>
2019-07-12 13:18:18 +0300

    Included .mat files and graphs.

--- src/main/scala/week2/rational/Experiment.scala
+++ src/main/scala/week2/rational/Experiment.scala
@@ -13,15 +13,14 @@ object Experiment extends App{
     val t0 = System.nanoTime()
     val result = block    // call-by-name
     val t1 = System.nanoTime()
-    println("Elapsed time for " + msg + " was " +  (t1 - t0) + "ns")
+    println("Elapsed time for " + msg + " in ns was " +  (t1 - t0))
     result
   }
 
-  println("======= EXPERIMENT 1: ACCURACY AND EFFICIENCY OF STANDARD MAP-REDUCE ========== ");
-
+   println("======= EXPERIMENT 1: ACCURACY AND EFFICIENCY OF STANDARD MAP-REDUCE ========== ");
   final val rng = new Random(47)
-  final val MAX_ITER = 1000
-  final val MAX_INT = 50   // Keep the numbers small for this exposition.
+  final val MAX_ITER = 10000
+  final val MAX_INT = 110   // Keep the numbers small for this exposition.
   val intTupleSeq : Seq[(Int, Int)]= for (_ <- 1 to MAX_ITER) yield (rng.nextInt(MAX_INT) + 1, rng.nextInt(MAX_INT) + 1)
   val quotientSeq : Seq[Double] = intTupleSeq map { case (a, b) => a / b.toDouble }
   val rationalSeq : Seq[Rational] = intTupleSeq map { case (a, b) => new Rational(a, b) }
@@ -29,27 +28,26 @@ object Experiment extends App{
   // Sums first....
   val quotientSum = time( quotientSeq.sum, "quotient sum")
   val rationalSum = time(rationalSeq.reduce((x, y) => x+y), "Rational sum")
-  println("Value of quotient sum:" + quotientSum)
+ // println("Value of quotient sum:" + quotientSum)
   val evaluatedRationalSum = rationalSum.eval
-  println("Value of Rational sum:" + evaluatedRationalSum)
-  println("Error: " + Math.abs(quotientSum - evaluatedRationalSum))
+ // println("Value of Rational sum:" + evaluatedRationalSum)
+  println("Sum error: " + Math.abs(quotientSum - evaluatedRationalSum))
 
   // Products second...
   val quotientProd = time( quotientSeq.product, "quotient product")
   val rationalProd = time(rationalSeq.reduce((x, y) => x*y), "Rational product")
-  println("Value of quotient product:" + quotientProd)
+ // println("Value of quotient product:" + quotientProd)
   val evaluatedRationalProd = rationalProd.eval
-  println("Value of Rational product:" + evaluatedRationalProd)
-  println("Error: " + Math.abs(quotientProd - evaluatedRationalProd))
-  
-  println("=========== EXPERIMENT 2: ACCURACY AND EFFICIENCY OF EXPONENTIATION ========= ")
-  final val EXPONENT = 200
-  val randomIdx = rng.nextInt(intTupleSeq.length)
+  //println("Value of Rational product:" + evaluatedRationalProd)
+  println("Product error: " + Math.abs(quotientProd - evaluatedRationalProd))
+
+  println("=========== EXPERIMENT 2: EFFICIENCY OF EXPONENTIATION ========= ")
+  final val EXPONENT = 500
   var quotientPower: Double = 0.0
   var rationalPower : Rational= _
-  time(quotientPower = scala.math.pow(quotientSeq(randomIdx), EXPONENT), "Double raised to the power of " + EXPONENT)
-  time(rationalPower = rationalSeq(randomIdx) ^ EXPONENT, "Rational raised to the power of " + EXPONENT)
+  time(quotientPower = scala.math.pow(1.5, EXPONENT), "Double raised to the power of " + EXPONENT)
+  time(rationalPower = new Rational(3, 2) ^ EXPONENT, "Rational raised to the power of " + EXPONENT)
   println("Value of power from Doubles: " + quotientPower + ".")
-  println("Value of power from Rationals" + rationalPower.eval + ".")
+  println("Value of power from Rationals: " + rationalPower.eval + ".")
   println("Error: " + Math.abs(quotientPower - rationalPower.eval))
 }
--- src/main/scala/week2/rational/Runner.scala
+++ src/main/scala/week2/rational/Runner.scala
@@ -4,8 +4,8 @@ object Runner extends App {
 
   private def fraction(a:Int, b:Int) = {
     require(b!= 0, "Cannot create a fraction with denominator zero.")
-     new Rational(a, b)
-     //a.toDouble / b.toDouble
+      new Rational(a, b)
+     a.toDouble / b.toDouble
   }
 
   val a = fraction(5, 6)
@@ -15,9 +15,6 @@ object Runner extends App {
   val e = fraction(25, 15) // Rational will reduce to 5/3
   val f = fraction(23, 13)
   val g = fraction(230, 17)
-  // println((a * b + c*d - e * (f + g)).eval)
+  println(a * b + c*d - e * (f + g))
 
-
-  val w:Int = 0
-  val z:Double = 1.3
 }
